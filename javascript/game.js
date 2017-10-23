1a7eb8355f0d0f3ad8ff2c4c598b4e8413fb32ff
Meeso Yang <meesoy@Meesos-MacBook-Pro.local>
2017-10-22 22:35:55 -0700

    more updates

--- javascript/game.js
+++ javascript/game.js
@@ -4,22 +4,153 @@
 // Alert the result at the end of game.
 
 
-var bandC = ["c","o","l","d","p","l","a","y"]
-var bandT = ["t","o","m","m","i","s","c","h"]
-var wins = 0
-var guessesRemaining = 15
-var lettersGuessed = []
+var wordsList = ["coldplay", "snowpatrol", "radiohead"];
+var chosenWord  = "";
+var lettersChosen = [];
+// number blaanks
+var numBlanks = 0;
+var blanksAndSucess = [];
+var letterWrong = []
 
 
+//
+var wins = 0;
+var loss = 0;
+var numGuesses = 10;
+
+
+
+function startGame(){
+	numGuesses = 10;
+	chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
+	
+	lettersChosen = chosenWord.split("");
+	console.log(lettersChosen);
+
+	/*for( var i = 0; i < chosenWord.length; i++){
+		lettersChosen.push(chosenWord[i]);
+	}*/
+
+	numBlanks = lettersChosen.length;
+
+	console.log(chosenWord);
+
+	blanksAndSucess = [];
+	letterWrong  = [];
+
+	for(var i = 0; i < numBlanks; i++){
+		blanksAndSucess.push("_");
+	}
+	console.log(blanksAndSucess);
+
+
+
+	document.getElementById("guesses-left").innerHTML = numGuesses;
+
+	document.getElementById("word-blanks").innerHTML = blanksAndSucess.join(" ");
+
+	document.getElementById("wrong-guess").innerHTML = letterWrong.join(" ");
+
+
+	//['c','o','l','p', 'l', 'a', 'y']
+}
+
+// c a t
+// c    
+// [ 'c' 'a' '_']
+function checkLetters(letter){
+
+
+	var letterInWord = false;
+	for(var i = 0; i < numBlanks; i++){
+		if(chosenWord[i] === letter){
+			letterInWord = true;
+		}
+	}
+
+
+	if(letterInWord === true){ // it is found 
+		for(var j = 0; j < numBlanks; j++){
+			if(chosenWord[j] === letter){
+				blanksAndSucess[j] = letter;
+			}
+		}
+	}
+	else{ // letterInWord false then wrong
+		letterWrong.push(letter);
+		numGuesses--;
+	}
+
+}
+
+
+
+function roundComplete(){
+	console.log("Wins: " + wins);
+	console.log("Loss: " + loss);
+	console.log("Number Guesses: " + numGuesses);
+
+	document.getElementById("guesses-left").innerHTML = numGuesses;
+
+
+	document.getElementById("word-blanks").innerHTML = blanksAndSucess.join(" ");
+
+
+	document.getElementById("wrong-guess").innerHTML = letterWrong.join(" ");
+
+	if(lettersChosen.toString() === blanksAndSucess.toString()){
+		win++;
+		alert("You win");
+		document.getElementById("win-counter").innerHTML = win;
+		startGame();
+	}
+	else if(numGuesses === 0){
+		loss++;
+		alert("You lose");
+		document.getElementById("loss-counter").innerHTML = loss;
+		startGame()
+	}
+
+}
+
+
+//var wins = 0
+//var guessesRemaining = 15
+
+
+//var lettersWrong = [];
+
+
+
+startGame();
 // Game will start by displaying objects when user presses a key.
 document.onkeyup = function(event) {
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
+	checkLetters(userGuess);
+
+	roundComplete();
+
+		/*console.log(userGuess);
+		for(var i = 0; i < bandC.length; i++ ){
+			if()
+		}
+		if(banC.indexOf(userGuess) >= 0){
+			lettersGuessed.push(userGuess);
+
+
+
+
+
+		}
+		else{
+			lettersWrong.push(userGuess);
+		}*/
 		// if ((userGuess === "c") || (userGuess === "o") || 
 		// 	(userGuess === "l") || (userGuess === "d") ||
 		// 	(userGuess === "p") || (userGuess === "l") ||
 		// 	(userGuess === "a") || (userGuess === "y")) {
-		if (inputtxt.value.match(bandC))
+		/*if (inputtxt.value.match(bandC))
 				wins++;
 				guessesRemaining--;
 				lettersGuessed.push(userGuess);
@@ -37,10 +168,10 @@ document.onkeyup = function(event) {
 			guessesRemaining = 15;
 			lettersGuessed = [];
 			}
-
+		*/
 	// Record wins, letters already guessed, # of guesses remaining (15 trials). # of guesses remaining will decrease.
 
-	var html = 
+	/*var html = 
 	"<h1>Hangman Game</h2>" +
 	"<p>Press any key to start playing!" +
 	"<p>Wins: " + wins + "</p>" +
@@ -49,7 +180,7 @@ document.onkeyup = function(event) {
 	"<p>Hints: Name of bands or singers</p>" +
 	"<p>Number of Guesses Remaining: " + guessesRemaining + "</p>" +
 	"<p>Letters Already Guessed: " + lettersGuessed + "</p>";
-
-	document.querySelector("#hangman-game").innerHTML = html;
+	*/
+	//document.querySelector("#hangman-game").innerHTML = html;
 
 	}
